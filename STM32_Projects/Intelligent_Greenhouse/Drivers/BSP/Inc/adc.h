f1760bdb437eb8254f7316ed646526352b9d22a6
undefined-0 <3047423259@qq.com>
2023-11-05 15:30:02 +0800

    完成ADC部分

--- STM32_Projects/Intelligent_Greenhouse/Drivers/BSP/Inc/adc.h
+++ STM32_Projects/Intelligent_Greenhouse/Drivers/BSP/Inc/adc.h
@@ -3,6 +3,6 @@
 #include "main.h"
 
 void ADC_Init(void);
-uint16_t ADC_Read(uint32_t channel);
+uint16_t ADC_Read(ADC_HandleTypeDef hadc, uint32_t channel);
 
 #endif /*__ADC_H__*/
--- STM32_Projects/Intelligent_Greenhouse/Drivers/BSP/Src/adc.c
+++ STM32_Projects/Intelligent_Greenhouse/Drivers/BSP/Src/adc.c
@@ -1,30 +1,50 @@
 #include "Drivers\BSP\Inc\adc.h"
 
-// 初始化ADC
-void ADC_Init(void) {
     ADC_HandleTypeDef hadc;
 
-    // 启用ADC时钟
-    __HAL_RCC_ADC1_CLK_ENABLE();
-
-    hadc.Instance = ADC1;
+void ADC_Init(void) 
+{
+    hadc.Instance = ADC1; //选择ADC1
 
     // ADC初始化结构体配置
-    hadc.Init.DataAlign = ADC_DATAALIGN_RIGHT; // 数据对齐方式设置为右对齐
-    hadc.Init.ScanConvMode = DISABLE; // 单通道模式
-    hadc.Init.ContinuousConvMode = DISABLE; // 单次转换
-    hadc.Init.NbrOfConversion = 1; // 转换的通道数为1
-    hadc.Init.DiscontinuousConvMode = DISABLE; // 不连续转换模式禁用
-    hadc.Init.NbrOfDiscConversion = 1; // 不连续转换的通道数为1
-    hadc.Init.ExternalTrigConv = ADC_SOFTWARE_START; // 外部触发转换使用软件触发方式启动
+    hadc.Init.DataAlign = ADC_DATAALIGN_RIGHT;          // 数据对齐方式设置为右对齐
+    hadc.Init.ScanConvMode = DISABLE;                   // 单通道模式
+    hadc.Init.ContinuousConvMode = DISABLE;             // 单次转换,非连续转换
+    hadc.Init.NbrOfConversion = 1;                      // 转换的通道数为1
+    hadc.Init.DiscontinuousConvMode = DISABLE;          // 不连续转换模式禁用
+    hadc.Init.NbrOfDiscConversion = 1;                  // 不连续转换的通道数为1
+    hadc.Init.ExternalTrigConv = ADC_SOFTWARE_START;    // 外部触发转换使用软件触发方式启动
 
     HAL_ADC_Init(&hadc);
+    HAL_ADCEx_Calibration_Start(&hadc);
+}
+
+HAL_ADC_MspInit(ADC_HandleTypeDef* hadc) //会被HAL_ADC_Init函数所调用的MSP回调函数，用来存放使能ADC和通道对应IO的时钟和初始化IO口等代码
+{
+if(hadc->Instance == ADC1)
+ {
+    RCC_PeriphCLKInitTypeDef adc_clk_init = {0};
+    GPIO_InitTypeDef gpio_init_struct;
+    
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    __HAL_RCC_ADC1_CLK_ENABLE();
+
+    // 设置 ADC 外设时钟
+    adc_clk_init.PeriphClockSelection = RCC_PERIPHCLK_ADC; // 选择 ADC 外设时钟为要配置的外设时钟
+    adc_clk_init.AdcClockSelection = RCC_ADCPCLK2_DIV6; // 将ADC的时钟分频设置为6，这意味着ADC的时钟频率将为主时钟（72MHz）除以6，即12MHz。
+    HAL_RCCEx_PeriphCLKConfig(&adc_clk_init); // 将ADC的时钟配置应用到硬件，告诉系统使用12MHz的时钟频率来驱动ADC模块。
+
+    // 设置 AD 采集通道对应 IO 引脚工作模式
+    gpio_init_struct.Pin = GPIO_PIN_1; // 选择引脚1为要配置的GPIO引脚
+    gpio_init_struct.Mode = GPIO_MODE_ANALOG; // 设置GPIO引脚的工作模式为模拟模式。在模拟模式下，该引脚将用于连接到ADC通道，以便进行模拟信号的采集
+    
+    HAL_GPIO_Init(GPIOA, &gpio_init_struct);
+ }
 }
 
 // 读取ADC值
-uint16_t ADC_Read(uint32_t channel) 
+uint16_t ADC_Read(ADC_HandleTypeDef hadc, uint32_t channel) 
 {
-    ADC_HandleTypeDef hadc;
     ADC_ChannelConfTypeDef sConfig; //通道配置结构体变量
 
     // 配置ADC通道
