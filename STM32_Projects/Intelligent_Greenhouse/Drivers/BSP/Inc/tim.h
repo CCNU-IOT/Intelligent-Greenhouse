d01ccf8c0ab6c47ccf9c11680e7e43e0d19cdfbd
undefined-0 <3047423259@qq.com>
2023-11-25 14:00:15 +0800

    将PWM输出引脚从PB3改至PE9并成功实现输出

--- STM32_Projects/Intelligent_Greenhouse/Drivers/BSP/Inc/tim.h
+++ STM32_Projects/Intelligent_Greenhouse/Drivers/BSP/Inc/tim.h
@@ -2,12 +2,14 @@
 #define __TIM_H__
 #include "main.h"
 
-#define WS2812B_PWM_TIM                           TIM2
+#define WS2812B_PWM_TIM                           TIM1
+
 #define WS2812B_PWM_TIM_PSC                       (72U - 1U)              /*72MHz -> 1MHz <---> 1us*/
 #define WS2812B_PWM_TIM_ARR                       (100U - 1U)             /*PWM 1M / 100 = 10KHz*/
-#define WS2812B_PWM_TIM_CH2_PORT                  GPIOB
-#define WS2812B_PWM_TIM_CH2_PIN                   GPIO_PIN_3
 
-void WS2812B_pwm_init(uint32_t duty);
+#define WS2812B_PWM_TIM_CH2_PORT                  GPIOE
+#define WS2812B_PWM_TIM_CH2_PIN                   GPIO_PIN_9
+
+void WS2812B_pwm_tim_init(uint32_t duty);
 
 #endif /*__TIM_H__*/
--- STM32_Projects/Intelligent_Greenhouse/Drivers/BSP/Src/tim.c
+++ STM32_Projects/Intelligent_Greenhouse/Drivers/BSP/Src/tim.c
@@ -18,25 +18,31 @@ void WS2812B_pwm_tim_init(uint32_t duty)
     tim_oc_config_struct.OCMode = TIM_OCMODE_PWM1; // 模式选择PWM1（TIMx_CNT<TIMx_CCR1时通道1为有效电平，否则为无效电平）
     tim_oc_config_struct.Pulse = duty; // 比较值（占空比）
     tim_oc_config_struct.OCNPolarity = TIM_OCNPOLARITY_HIGH; // 设置通道极性为高电平有效
-    HAL_TIM_PWM_ConfigChannel(&WS2812B_pwm_tim_handle, &tim_oc_config_struct, TIM_CHANNEL_2); // 配置定时器通道2为PWM模式
-    HAL_TIM_PWM_Start(&WS2812B_pwm_tim_handle, TIM_CHANNEL_2); // 使能定时器通道2
+    HAL_TIM_PWM_ConfigChannel(&WS2812B_pwm_tim_handle, &tim_oc_config_struct, TIM_CHANNEL_1); // 配置定时器通道1为PWM模式
+    // HAL_TIM_PWM_ConfigChannel(&WS2812B_pwm_tim_handle, &tim_oc_config_struct, TIM_CHANNEL_2); // 配置定时器通道2为PWM模式
+    // HAL_TIM_PWM_ConfigChannel(&WS2812B_pwm_tim_handle, &tim_oc_config_struct, TIM_CHANNEL_3); // 配置定时器通道3为PWM模式
+    // HAL_TIM_PWM_ConfigChannel(&WS2812B_pwm_tim_handle, &tim_oc_config_struct, TIM_CHANNEL_4); // 配置定时器通道4为PWM模式
+    HAL_TIM_PWM_Start(&WS2812B_pwm_tim_handle, TIM_CHANNEL_1); // 使能定时器通道1
+    // HAL_TIM_PWM_Start(&WS2812B_pwm_tim_handle, TIM_CHANNEL_2); // 使能定时器通道2
+    // HAL_TIM_PWM_Start(&WS2812B_pwm_tim_handle, TIM_CHANNEL_3); // 使能定时器通道3
+    // HAL_TIM_PWM_Start(&WS2812B_pwm_tim_handle, TIM_CHANNEL_4); // 使能定时器通道4
 }
 
 void HAL_TIM_PWM_MspInit(TIM_HandleTypeDef *htim) // 此函数会被HAL_TIM_PWM_Init()调用
 {
     if (htim->Instance == WS2812B_PWM_TIM)
     {
-        __HAL_RCC_GPIOB_CLK_ENABLE(); // 为使用定时器2通道2（重映射至PB3），使能GPIOB时钟
-        __HAL_RCC_TIM2_CLK_ENABLE(); // 使能TIM2时钟
+        __HAL_RCC_GPIOE_CLK_ENABLE(); // 为使用定时器1通道1（重映射至PE9），使能GPIOE时钟
+        __HAL_RCC_TIM1_CLK_ENABLE(); // 使能TIM1时钟
         __HAL_RCC_AFIO_CLK_ENABLE(); // 使能AFIO时钟
 
         GPIO_InitTypeDef gpio_init_struct;
-        gpio_init_struct.Pin = WS2812B_PWM_TIM_CH2_PIN; // 设置GPIO引脚为WS2812B_PWM_TIM_CH2_PIN（GPIO_PIN_3）
-        gpio_init_struct.Mode = GPIO_MODE_AF_PP; // 复用推挽输出,作为TIM2的CH2输出
+        gpio_init_struct.Pin = WS2812B_PWM_TIM_CH2_PIN; // 设置GPIO引脚为WS2812B_PWM_TIM_CH2_PIN（GPIO_PIN_9）
+        gpio_init_struct.Mode = GPIO_MODE_AF_PP; // 复用推挽输出,作为TIM1的CH2输出
         gpio_init_struct.Pull = GPIO_NOPULL;           
         gpio_init_struct.Speed = GPIO_SPEED_FREQ_HIGH;     
         HAL_GPIO_Init(WS2812B_PWM_TIM_CH2_PORT, &gpio_init_struct);
 
-        __HAL_AFIO_REMAP_TIM2_ENABLE(); // 启用TIM2的重映射
+        __HAL_AFIO_REMAP_TIM1_ENABLE(); // 启用TIM1的重映射
     }
 }
